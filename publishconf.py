3bd9314ba7004f9b691194434a4343a5596028e5
Tiago Gomes <tiago.gomes@codethink.co.uk>
2018-08-29 17:53:58 +0100

    Fix SITEURL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = ''
+SITEURL = 'https://buildstream.gitlab.io/website'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
