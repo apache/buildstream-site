fda1ccfd443c80562de3f21f01b3ea29a356adac
Tiago Gomes <tiago.gomes@codethink.co.uk>
2018-08-30 12:58:53 +0100

    Update SITEURL again

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://buildstream.gitlab.io/website'
+SITEURL = 'https://buildstream.build'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
